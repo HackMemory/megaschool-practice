b6d5d2b5008489c460c365319c16649b14ad7395
HackMemory <dragon.warri0r@yandex.com>
2024-02-16 20:18:18 +0300

    feat: added glide & sdp deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //glide
+    implementation 'com.github.bumptech.glide:glide:4.16.0'
+    implementation "androidx.activity:activity-ktx:1.3.1"
+
+    //sdp
+    implementation 'com.intuit.sdp:sdp-android:1.1.0'
+    implementation 'com.intuit.ssp:ssp-android:1.1.0'
 }
